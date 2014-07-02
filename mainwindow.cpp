5d0629c1e570ef71fa5171380976e14173e126c2
kamalpreetgrewal <kkamalpreet96@yahoo.in>
2014-07-02 19:15:37 +0530

    paints a line

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,17 +1,30 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QRect>
+#include <QPainter>
+#include <QMouseEvent>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-        setWindowTitle(tr("GD CAD"));
+    setWindowTitle(tr("GD CAD"));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+
+    //a simple line
+    painter.drawLine(100,100,200,200);
+
 }
 
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,11 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QString>
-#include <QFileDialog>
-#include <QMessageBox>
-#include <QFile>
-#include <QTextStream>
+#include <QPaintEvent>
 
 namespace Ui {
 class MainWindow;
@@ -20,8 +16,13 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+protected:
+    void paintEvent(QPaintEvent *event);
+
 private:
     Ui::MainWindow *ui;
+
+
 };
 
 #endif // MAINWINDOW_H
