f0423fe4aed7f1ea9dbb1045995b9c12666d6a38
kamalpreetgrewal <kkamalpreet96@yahoo.in>
2014-07-08 23:48:52 +0530

    implements mouse tracking for line

--- line.cpp
+++ line.cpp
@@ -1,8 +1,13 @@
 #include "line.h"
+#include <QDebug>
 
 line::line()
 {
-
+    x1 = 0;
+    y1 = 0;
+    x2 = 0;
+    y2 = 0;
+    mFirstClick = true;
 }
 
 QRectF line::boundingRect() const
@@ -10,10 +15,57 @@ QRectF line::boundingRect() const
     // outer most edges
     return QRectF(0,0,100,100);
 }
+void line::mousePressEvent(QGraphicsSceneMouseEvent* e){
+ if(e->button()==Qt::LeftButton) {
+     if(mFirstClick){
+         x1 = e->pos().x();
+         y1 = e->pos().y();
+         mFirstClick = false;
+     }
+
+     else if(!mFirstClick){
+         x2 = e->pos().x();
+         y2 = e->pos().y();
+         mFirstClick = true;
+         mPaintFlag = true;
+         update();
+     }
+}
+}
+
 
 void line:: paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget){
     QRectF rect = boundingRect();
-    QPen pen(Qt::black, 1);
-    painter->setPen(pen);
-    painter->drawLine(100,100,20,200);
+        QPen pen(Qt::black, 1);
+    if(mPaintFlag){
+
+        QPen paintpen(Qt::red);
+        paintpen.setWidth(1);
+
+        QPen linepen(Qt::black);
+        linepen.setWidth(1);
+
+        QPoint p1;
+        p1.setX(x1);
+        p1.setY(y1);
+
+        painter->setPen(paintpen);
+        painter->drawPoint(p1);
+
+        QPoint p2;
+        p2.setX(x2);
+        p2.setY(y2);
+
+        painter->setPen(paintpen);
+        painter->drawPoint(p2);
+
+        painter->setPen(linepen);
+        painter->drawLine(p1, p2);
+    }
 }
+
+//void line::wheelEvent(QGraphicsSceneWheelEvent *e){
+//    m_scale += e->delta() / qreal(600);
+//        m_scale = qMax(qreal(0.1), qMin(qreal(4), m_scale));
+//        emit scaleChanged(int(m_scale*1000));
+//}
--- line.h
+++ line.h
@@ -3,15 +3,41 @@
 
 #include <QPainter>
 #include <QGraphicsItem>
+#include <QMouseEvent>
+#include <QDebug>
+#include <QGraphicsSceneMouseEvent>
+#include "ui_mainwindow.h"
 
-class line:public QGraphicsItem
+class line: public QGraphicsItem
 {
 public:
     line();
     QRectF boundingRect() const;
-    virtual void paint(QPainter * painter,
-                       const QStyleOptionGraphicsItem * option,
-                       QWidget * widget);
+    virtual void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+    qreal scale() const { return m_scale; }
+    void setScale(qreal s);
+
+
+protected:
+    void mousePressEvent(QGraphicsSceneMouseEvent *e);
+    //void wheelEvent(QGraphicsSceneWheelEvent *e);
+public slots:
+    //void ZoomIn{scale(1.4,1.4)};
+    void changeScale(int scale);
+
+
+signals:
+    void scaleChanged(int scale);
+
+
+private:
+    int x1, y1, x2, y2;
+    bool mFirstClick;
+    bool mPaintFlag;
+    bool Pressed;
+    QPoint *mousePoint;
+    Ui::MainWindow *ui;
+    qreal m_scale;
 };
 
 #endif // LINE_H
