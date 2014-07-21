96339c4869b85f323b20478ec23f1a0a11c9b1a9
kamalpreetgrewal <kkamalpreet96@yahoo.in>
2014-07-21 23:06:01 +0530

    removes grid

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -41,26 +41,6 @@ bool MainWindow::eventFilter(QObject *obj, QEvent *event)
     return false;
 }
 
-void MainWindow::paintGrid(QPainter* painter, const QRectF& rect)
-{
-    int gridInterval = 100; //interval to draw grid lines at
-    painter->setWorldMatrixEnabled(true);
-
-    qreal left = int(rect.left()) - (int(rect.left()) % gridInterval );
-    qreal top = int(rect.top()) - (int(rect.top()) % gridInterval );
-
-    QVarLengthArray<QLineF, 100> linesX;
-    for (qreal x = left; x < rect.right(); x += gridInterval )
-        linesX.append(QLineF(x, rect.top(), x, rect.bottom()));
-
-    QVarLengthArray<QLineF, 100> linesY;
-    for (qreal y = top; y < rect.bottom(); y += gridInterval )
-        linesY.append(QLineF(rect.left(), y, rect.right(), y));
-
-    painter->drawLines(linesX.data(), linesX.size());
-    painter->drawLines(linesY.data(), linesY.size());
-}
-
 void  MainWindow::filePrintPreview()
 {
     // display print preview dialog
